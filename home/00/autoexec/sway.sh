0bebc6bfafc515a2d7aef068f52049ce85b9715c
oo <CYPHER0@foxmail.com>
2026-03-01 16:56:46 +0800

    update

--- home/00/autoexec/sway.sh
+++ home/00/autoexec/sway.sh
@@ -1,12 +1,26 @@
 #!/usr/bin/env bash
 
 
-sleep 1
-swaymsg focus output "DP-5"
-firefox &
+pulseaudio --kill &
 
-sleep 1
-swaymsg focus output "DP-1"
-alacritty -e tmux &
+OUTPUT_HDMI=$(swaymsg -t get_outputs |grep -i 'HDMI')
+OUTPUT_DP=$(swaymsg -t get_outputs |grep -i 'DP')
+
+if [ -n "$OUTPUT_HDMI" ]; then
+  sleep 1
+  swaymsg workspace "1"
+  firefox &
+  sleep 1
+  swaymsg workspace "2"
+  alacritty -e tmux &
+elif [ -n "$OUTPUT_DP" ]; then
+  sleep 1
+  swaymsg focus output "DP-5"
+  firefox &
+  sleep 1
+  swaymsg focus output "DP-1"
+  alacritty -e tmux &
+fi
+
 
 
